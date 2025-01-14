720dd05346aa8042f626292d2bb22c5e42b44218
HasChad <troll.yahya@gmail.com>
2025-01-14 21:38:43 +0300

    - fixed bugs - added zoom to camera center instead of world center - added precision to camera zoom

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use macroquad::prelude::*;
 
+const ZOOM_DEFAULT: f32 = 2.0; // change this if you want different starting zoom level
+
 pub fn window_conf() -> Conf {
     Conf {
         window_title: "Macroquad Freedom Camera2D".into(),
@@ -20,7 +22,7 @@ async fn main() {
 
     // variable for zooming in and out
     // you can remove this if you dont plan to use camera zoom
-    let mut zoomer = Vec2::ZERO;
+    let mut zoomer = ZOOM_DEFAULT;
 
     loop {
         camera_fixer(&mut camera, &mut zoomer);
@@ -40,20 +42,17 @@ async fn main() {
             ORANGE,
         );
 
-        draw_triangle(vec2(50.0, -50.0), vec2(25.0, 0.0), vec2(75.0, 0.0), BLUE);
+        draw_rectangle(100.0, texture_size / -2., texture_size, texture_size, BLUE);
 
         next_frame().await;
     }
 }
 
-fn camera_fixer(camera: &mut Camera2D, zoomer: &mut Vec2) {
-    // disable stretch when window size is changed
-    camera.zoom = vec2(
-        2. / screen_width() + zoomer.x / screen_width(),
-        2. / screen_height() + zoomer.y / screen_height(),
-    );
+fn camera_fixer(camera: &mut Camera2D, zoomer: &mut f32) {
+    // resize camera width and height to window width and height
+    camera.zoom = vec2(*zoomer / screen_width(), *zoomer / screen_height());
 
-    // set min window heigh and width
+    // set min window width and heigh
     if screen_width() < 320. {
         request_new_screen_size(320., screen_height());
     }
@@ -64,11 +63,21 @@ fn camera_fixer(camera: &mut Camera2D, zoomer: &mut Vec2) {
 
     // zoom in
     if mouse_wheel().1 > 0. {
-        *zoomer += 0.2
+        // way to always get precise .2 accuracy for floating point operation
+        *zoomer = (*zoomer * 10.).round() / 10.;
+
+        *zoomer += 0.2;
 
     // zoom out
-    } else if mouse_wheel().1 < 0. && zoomer.x > -1. {
+    } else if mouse_wheel().1 < 0. && *zoomer > 0.2 {
+        // way to always get precise .2 accuracy for floating point operation
+        *zoomer = (*zoomer * 10.).round() / 10.;
+
         *zoomer -= 0.2;
+
+        if *zoomer < 0.2 {
+            *zoomer = 0.2;
+        }
     }
 
     // move camera
@@ -76,13 +85,13 @@ fn camera_fixer(camera: &mut Camera2D, zoomer: &mut Vec2) {
         let mouse_pos = mouse_delta_position();
 
         // you can change the add and sub assigments if you plan to use different camera movement
-        camera.offset.x -= mouse_pos.x;
-        camera.offset.y += mouse_pos.y;
+        camera.target.x += mouse_pos.x * screen_width() / *zoomer;
+        camera.target.y += mouse_pos.y * screen_height() / *zoomer;
     }
 
     // reset position and zoom
     if is_key_pressed(KeyCode::Space) {
-        camera.offset = Vec2::ZERO;
-        *zoomer = Vec2::ZERO;
+        camera.target = Vec2::ZERO;
+        *zoomer = ZOOM_DEFAULT;
     }
 }
